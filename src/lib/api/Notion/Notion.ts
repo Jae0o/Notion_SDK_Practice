0f3f29c41556d15a0d6626b02c6345800b12995c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-02-10 09:15:41 +0900

    ♻️ Refactor :  Notion SDK 사용 불가에 따른 임시 API 생성

--- src/lib/api/Notion/Notion.ts
+++ src/lib/api/Notion/Notion.ts
@@ -1,9 +1,32 @@
-import { Client } from "@notionhq/client";
-
 const { VITE_NOTION_API_KEY } = import.meta.env;
 
-const Notion = new Client({
-  auth: VITE_NOTION_API_KEY,
-});
+// const Notion11 = new Client({
+//   auth: VITE_NOTION_API_KEY,
+// });
+
+interface createExamplePrams {
+  parent: {
+    database_id: string;
+    type: "database_id" | "page_id";
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  properties: any;
+}
+
+const Notion = {
+  pages: {
+    create: async (param: createExamplePrams) => {
+      return await fetch("/notion/v1/pages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${VITE_NOTION_API_KEY}`,
+          "Notion-Version": "2022-06-28",
+        },
+        body: JSON.stringify(param),
+      });
+    },
+  },
+};
 
 export default Notion;
